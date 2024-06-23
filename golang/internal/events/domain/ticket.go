de5216073152e4b3dbde83db7a4539f70ab7f627
Henrique Barucco <henrique_barucco@hotmail.com>
2024-06-23 20:20:10 +0000

    feat: create usecases

--- golang/internal/events/domain/ticket.go
+++ golang/internal/events/domain/ticket.go
@@ -1,40 +1,62 @@
 package domain
 
-import "errors"
+import (
+	"errors"
 
-type TicketType string
+	"github.com/google/uuid"
+)
 
 var (
-	ErrTicketPriceZero = errors.New("ticket price must be greater than zero")
+	ErrInvalidTicketKind = errors.New("invalid ticket kind")
 )
 
+type TicketKind string
+
 const (
-	TicketTypeHalf TicketType = "half"
-	TicketTypeFull TicketType = "full"
+	TicketKindHalf TicketKind = "half"
+	TicketKindFull TicketKind = "full"
 )
+
+func IsValidTicketKind(ticketKind TicketKind) bool {
+	return ticketKind == TicketKindHalf || ticketKind == TicketKindFull
+}
 
 type Ticket struct {
 	ID         string
 	EventID    string
 	Spot       *Spot
-	TicketType TicketType
+	TicketKind TicketKind
 	Price      float64
 }
 
-func IsValidTicketType(ticketType TicketType) bool {
-	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+func NewTicket(event *Event, spot *Spot, ticketKind TicketKind) (*Ticket, error) {
+	if !IsValidTicketKind(ticketKind) {
+		return nil, ErrInvalidTicketKind
+	}
+
+	ticket := &Ticket{
+		ID:         uuid.New().String(),
+		EventID:    event.ID,
+		Spot:       spot,
+		TicketKind: ticketKind,
+		Price:      event.Price,
+	}
+	ticket.CalculatePrice()
+	if err := ticket.Validate(); err != nil {
+		return nil, err
+	}
+	return ticket, nil
 }
 
 func (t *Ticket) CalculatePrice() {
-	if t.TicketType == TicketTypeHalf {
+	if t.TicketKind == TicketKindHalf {
 		t.Price /= 2
 	}
 }
 
 func (t *Ticket) Validate() error {
 	if t.Price <= 0 {
-		return ErrTicketPriceZero
+		return errors.New("ticket price must be greater than zero")
 	}
-
 	return nil
 }
